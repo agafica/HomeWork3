adc1e58cf8e886d5482a7aa5d57752b14175491d
Albina <sagafio@mail.ru>
2023-07-15 14:14:17 +0300

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿
+//Task2. Напишите программу, которая
+// принимает на вход координаты двух точек и находит 
+//расстояние между ними в 3D пространстве.
+
+/*double Hyp(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double result = Math.Sqrt(x2 - x1)*(x2-x1) + ((y2-y1)*(y2-y1))+((z2-z1)*(z2-z1));
+    return result; 
+}
+Console.Write("Input X1: ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Y1: ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Z1: ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input X2: ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Y2: ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Z2: ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(Hyp(X1,Y1,Z1,X2,Y2,Z2));
+
+//Task3. Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N
+
+//Домашняя работа задача 2
+/*void paw2(int paw)
+{
+    int count = 0;
+    while(count <= paw)
+    {
+        Console.WriteLine($"{count}  -  {count * count * count}");
+        count++;
+    }
+}
+Console.Write("Input number: ");
+int paw = Convert.ToInt32(Console.ReadLine());
+
+paw2(paw);*/
+
+//.Метод типа БООЛ. Сформировать перевернутую версию процент сформировать обратное сравнить с исходным
+//2.Аналог второй задачи появлется зет
+//3. Выдает таблицу кубов//
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+/*bool check_polindrom(string word)
+{
+    if(string == 2 ) return true;
+    else return false;
+}
+
+Console.Write("Input a number for checking: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+bool result = check_polindrom(number);
+Console.WriteLine(result);*/
